c9bcef23a3ad8f619e9564390d7342fb35e851bd
Wyes Karny <wkarny@gmail.com>
2017-03-19 22:30:06 +0530

    Added

--- gateway.cpp
+++ gateway.cpp
@@ -16,6 +16,9 @@
 
 using namespace std;
 
+#define MAIN_SERVER_IP_ADDR "192.168.0.106"
+#define MAIN_SERVER_PORT_NUM 8080
+
 //message type
 #define ADD_REQ 0
 #define ATH_REQ 1
@@ -137,35 +140,39 @@ void socket_thread(){
     struct sockaddr_in address;
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
+    char *hello = "UserID: RPI01 PassWD: 2810";
     char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)   //Should be replaced by while
     {
-        printf("\n Socket creation error \n");
-        return;
+      cout<<"Socket creation error\n";
+      return;
     }
   
     memset(&serv_addr, '0', sizeof(serv_addr));
   
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(8080);
+    serv_addr.sin_port = htons(MAIN_SERVER_PORT_NUM);                  //Port Address
       
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "192.168.0.106", &serv_addr.sin_addr)<=0) 
+    if(inet_pton(AF_INET, MAIN_SERVER_IP_ADDR, &serv_addr.sin_addr)<=0)  //Server IP Address
     {
-        printf("\nInvalid address/ Address not supported \n");
-        return;
+      cout<<"Invalid address\n";
+      return;
     }
   
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        printf("\nConnection Failed \n");
-        return;
+      cout<<"Connection Failed \n";
+      return;
     }
     send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
-    valread = read( sock , buffer, 1024);
-    printf("%s\n",buffer );
+    cout<<"Auth message sent\n";
+    memset(buffer,0,sizeof(buffer));
+    while(1){
+      valread = read( sock , buffer, 1024);
+      cout<<"Recived from Socket: "<<buffer<<endl;
+      memset(buffer,0,sizeof(buffer));
+    }
+
 }
 
 int main(){
