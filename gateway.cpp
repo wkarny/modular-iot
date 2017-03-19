754ca1cf03cc56854963925bd06e615ff1c50cea
Wyes Karny <wkarny@gmail.com>
2017-03-19 21:05:57 +0530

    Added

--- gateway.cpp
+++ gateway.cpp
@@ -4,6 +4,14 @@
 #include <cstdlib>
 #include <RF24/RF24.h>
 #include <thread>
+
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netdb.h>
+
 using namespace std;
 
 //message type
@@ -124,10 +132,38 @@ RF24 radio(22,0);
 uint16_t last_tid=200;           //should be replaced with non-volatile storage
 
 void socket_thread(){
-  while(1){
-    cout<<"Socket Thread"<<endl;
-    //sleep(1);
-  }
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+      
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "192.168.0.106", &serv_addr.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    valread = read( sock , buffer, 1024);
+    printf("%s\n",buffer );
 }
 
 int main(){
